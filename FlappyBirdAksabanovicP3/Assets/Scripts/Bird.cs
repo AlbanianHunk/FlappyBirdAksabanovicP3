3cf80559f694c23a23e5927b5dd01ad1809a360a
AlbanianHunk <116110355+AlbanianHunk@users.noreply.github.com>
2022-12-15 11:43:15 -0800

    Bird Script + Animation
    Worked on the bird script and set the animations for idle, flap and dead. started the Ui section

---
+++ FlappyBirdAksabanovicP3/Assets/Scripts/Bird.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce;
+    private bool isDead = false;
+
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        anim = GetComponent<Animator> ();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Don't allow control if the bird has died.
+        if (isDead == false)
+        {
+            //Look for input to trigger a "flap".
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Flap");
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Dead");
+    }
+}
